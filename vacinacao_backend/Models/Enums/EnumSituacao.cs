6f129bc4cd0012d4102d8d937f4be0dfd289a45a
joaopedrosf <joaopedrosf1903@gmail.com>
2023-06-26 19:55:15 -0300

    Enum convert to string

--- vacinacao_backend/Models/Enums/EnumSituacao.cs
+++ vacinacao_backend/Models/Enums/EnumSituacao.cs
@@ -1,4 +1,7 @@
-﻿namespace vacinacao_backend.Models.Enums {
+﻿using System.Text.Json.Serialization;
+
+namespace vacinacao_backend.Models.Enums {
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum EnumSituacao {
         Agendado,
         Realizado,
